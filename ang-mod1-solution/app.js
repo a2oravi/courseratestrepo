b39eb7f14778bcb7825f31be627e38361f278357
rvdesaigit <ravi@a2omobile.com>
2019-12-21 23:58:51 +0530

    added

---
+++ ang-mod1-solution/app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  //$scope.dishes = "enter comma seperated dishes";
+  $scope.lunchMessage = "some message";
+
+  $scope.checkDishes = function () {
+  	console.log("got dishes = " + $scope.dishes);
+  	if ($scope.dishes == null || $scope.dishes == "") {
+  		$scope.lunchMessage = "Please enter data first";
+  	} else {
+  		var res = $scope.dishes.split(",");
+  		console.log("Got array length = " + res.length);
+  		if (res.length <= 3) {
+			$scope.lunchMessage = "Enjoy !";
+  		} else {
+  			$scope.lunchMessage = "Too much !";
+  		}
+  	}
+  };
+
+}
+
+})();
